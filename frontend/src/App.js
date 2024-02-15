a445bed1481bd12fa5cbc44517016dd502e42bde
prajwal raut <90103440+Prajwalraut29@users.noreply.github.com>
2024-02-15 14:53:19 +0530

    link add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://backend-chat-3695.onrender.com");
 function App() {
   const [username, setusername] = useState("");
   const [chatActive, setChatActive] = useState(false);
