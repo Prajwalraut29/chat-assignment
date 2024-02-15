0938c23a5924d6067a469102096b9e729f89d02b
prajwal raut <90103440+Prajwalraut29@users.noreply.github.com>
2024-02-15 17:41:41 +0530

    link

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
   cors:{
-    origin:'https://chat-assignment.vercel.app/',
+    origin:'https://app-frontend-chat.onrender.com',
     methods :["GET","POST"]
   },
 })
